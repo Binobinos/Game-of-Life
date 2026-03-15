d9fee0e85dfbd0f08e85b422df0f1ea8f6c4a655
SashaTrof1mov <noobly7.com@gmail.com>
2026-03-15 15:28:25 +0100

    добавил папку

--- game-of-life-pyGame/renderer.py
+++ game-of-life-pyGame/renderer.py
@@ -1,6 +1,12 @@
 """file: renderer.py"""
+from display import Display
+import pygame
 
 class Renderer:
 
     def __init__(self):
+        self.display = Display()
+        self.screen = pygame.display.set_mode((self.display.WIDTH, self.display.HEIGHT))
+
+    def update(self):
         pass
