41514586577c80a1c67ebd2fc600d9ff95ee9ef0
SashaTrof1mov <noobly7.com@gmail.com>
2026-03-15 16:59:23 +0100

    fps_tick

--- game-of-life-pyGame/renderer.py
+++ game-of-life-pyGame/renderer.py
@@ -7,8 +7,26 @@ class Renderer:
     def __init__(self):
         self.display = Display()
         self.screen = pygame.display.set_mode((self.display.WIDTH, self.display.HEIGHT))
+        self.text = Text(self.screen)
 
     def update(self):
         """рендер"""
         self.screen.fill((255, 255, 255))
+        self.text.render()
         pygame.display.flip()
+
+
+class Text:
+
+    def __init__(self, screen):
+        self.screen = screen
+        self._cache = {}
+
+    def render(self, font="Arial", text="None", color=(0, 0, 0), x=0, y=0, size=30):
+        key = (text, color, font, size)
+        if key not in self._cache:
+            font = pygame.font.SysFont(font, size)
+            text_surface = font.render(str(text), True, color)
+            self._cache[key] = text_surface
+        self.screen.blit(self._cache[key], (x, y))
+
