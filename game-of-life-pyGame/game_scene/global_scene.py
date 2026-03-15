08050e52b4db600e9dbcc716708bdc5a25863412
SashaTrof1mov <noobly7.com@gmail.com>
2026-03-15 17:22:43 +0100

    .

--- game-of-life-pyGame/game_scene/global_scene.py
+++ game-of-life-pyGame/game_scene/global_scene.py
@@ -12,5 +12,6 @@ class Scene0:
 
     def render(self, dt, clock):
         self.screen.fill(self.screen_color)
-        self.text.render(text="Hello World")
-        self.text.render(text="Game - Of - Life", x=500)
+        self.text.render(text="Hello World", y=50)
+        self.text.render(text="Game - Of - Life", x=450, size=50, color=(255, 12, 12))
+        self.text.render(text=f"fps: {int(clock.get_fps())}")
